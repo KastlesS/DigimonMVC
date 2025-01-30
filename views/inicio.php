cd12399bfd55ef9aa6433ac48ac785c4cac748c1
Ivan Castillo <ivacassim@alu.edu.gva.es>
2025-01-30 20:12:38 +0100

    calla

--- views/inicio.php
+++ views/inicio.php
@@ -6,6 +6,20 @@ if (!isset($_SESSION['user'])) {
     exit();
 }
 
-$user = $_SESSION['']
-
+$user = $_SESSION['user'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>users</title>
+</head>
+<body>
+    <div class="buttons">
+        <button onclick="window.location.href='/index.php?tabla=user&accion=listar'">Ver Usuarios</button>
+        <button onclick="window.location.href='/index.php?tabla=digimon&accion=listar'">Ver Digimones</button>
+    </div>
+</body>
+</html>
